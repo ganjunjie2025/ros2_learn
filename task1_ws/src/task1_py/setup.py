11aed6252826ac61b1791e83c746c144518a2fd9
ganjunjie2025 <3823969377@qq.com>
2026-03-05 19:10:03 +0800

    task1

---
+++ task1_ws/src/task1_py/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'task1_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gan',
+    maintainer_email='gan@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'pub_node=task1_py.pub_num:main',
+            'sub_node=task1_py.sub_num:main'
+        ],
+    },
+)
